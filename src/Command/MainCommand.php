c6bf87658ae0ab5d2b65f1d68929de68da3edc87
Vladimir Glechikov <glevolod@gmail.com>
2021-07-16 15:00:18 +0300

    Add logging

--- src/Command/MainCommand.php
+++ src/Command/MainCommand.php
@@ -5,6 +5,7 @@ namespace Glevolod\ChainCommandBundle\Command;
 
 
 use Glevolod\ChainCommandBundle\Service\CommandChain;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
@@ -14,11 +15,18 @@ class MainCommand extends Command
 {
     protected static $defaultName = 'glevolod:main-command';
 
-    private $commandChain;
+    private CommandChain $commandChain;
+    private LoggerInterface $logger;
 
-    public function __construct(CommandChain $commandChain)
+    /**
+     * MainCommand constructor.
+     * @param CommandChain $commandChain
+     * @param LoggerInterface $logger
+     */
+    public function __construct(CommandChain $commandChain, LoggerInterface $logger)
     {
         $this->commandChain = $commandChain;
+        $this->logger = $logger;
         parent::__construct();
     }
 
@@ -27,15 +35,20 @@ class MainCommand extends Command
         $this
             ->setDescription('Runs main command and other commands added to the chain')
             ->setHelp('Other commands should be registered with tags');
+        $this->logger->info(self::$defaultName . ' is a master command of a command chain that has registered member commands');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $this->logger->info('Executing ' . self::$defaultName . ' command itself first:');
         $output->writeln('Hello from command "' . self::$defaultName . '"');
+        $this->logger->info('Executing ' . self::$defaultName . ' as a chain members:');
         /** @var Command $command */
         foreach ($this->commandChain->getCommands() as $command) {
+            $this->logger->info('Executing ' . $command->getName() . ' chain member');
             $command->run(new ArrayInput([]), $output);
         }
+        $this->logger->info('Executing of ' . self::$defaultName . ' chain completed.');
         return Command::SUCCESS;
     }
 }
--- src/Service/CommandChain.php
+++ src/Service/CommandChain.php
@@ -4,19 +4,24 @@
 namespace Glevolod\ChainCommandBundle\Service;
 
 
+use Glevolod\ChainCommandBundle\Command\MainCommand;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 
 class CommandChain
 {
     private $commands;
+    private LoggerInterface $logger;
 
-    public function __construct()
+    public function __construct(LoggerInterface $logger)
     {
         $this->commands = [];
+        $this->logger = $logger;
     }
 
     public function addCommand(Command $command): void
     {
+        $this->logger->info($command->getName() . ' registered as a member of ' . MainCommand::getDefaultName() . ' command chain');
         $this->commands[] = $command;
     }
 
